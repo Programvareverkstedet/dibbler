993ff0e0129c5b4e6cba34bcc85d6e70be0ff0d6
almelid <almelid@PVV.NTNU.NO>
2010-05-18 19:25:01 +0000

    litt flere quit-kommandoer

--- text_based.py
+++ text_based.py
@@ -10,7 +10,7 @@ import signal
 import readline
 from helpers import *
 
-exit_commands = ['exit', 'abort', 'quit', 'bye', 'eat flaming death']
+exit_commands = ['exit', 'abort', 'quit', 'bye', 'eat flaming death', 'q']
 help_commands = ['help', '?']
 context_commands = ['what', '??']
 local_help_commands = ['help!', '???']
