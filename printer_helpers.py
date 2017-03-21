806c9c35162e26e45e284b7c7ea92a74326c96b6
Christoffer Viken <christoffer@viken.me>
2017-03-21 21:08:19 +0000

    No idae what happened, but rotate in PIL instead of brother

--- printer_helpers.py
+++ printer_helpers.py
@@ -9,17 +9,17 @@ from barcode_helpers import BrotherLabelWriter
 def print_bar_code(barcode_value, barcode_text, barcode_type="ean13", rotate=False, printer_type="QL-700",
                    label_type="62"):
     bar_coder = barcode.get_barcode_class(barcode_type)
-    wr = BrotherLabelWriter(typ=label_type, rot=rotate*90, text=barcode_text, max_height=1000)
+    wr = BrotherLabelWriter(typ=label_type, rot=rotate, text=barcode_text, max_height=1000)
 
     test = bar_coder(barcode_value, writer=wr)
     fn = test.save(barcode_value)
-    print_image(fn, printer_type, label_type, rotate*90)
+    print_image(fn, printer_type, label_type)
 
 
-def print_image(fn, printer_type="QL-700", label_type="62", rotate=0):
+def print_image(fn, printer_type="QL-700", label_type="62"):
     qlr = BrotherQLRaster(printer_type)
     qlr.exception_on_warning = True
-    create_label(qlr, fn, label_type, threshold=70, cut=True, rotate=rotate)
+    create_label(qlr, fn, label_type, threshold=70, cut=True)
 
     be = backend_factory("pyusb")
     list_available_devices = be['list_available_devices']
