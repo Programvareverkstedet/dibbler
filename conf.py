1c8f03695ae5adc20108666d4f0f4c0d4e67cf63
Christoffer Viken <christoffer@viken.me>
2017-04-15 18:47:28 +0000

    Cleanup

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-db_url = 'sqlite:///foo.db'
+db_url = 'postgres://CVi@127.0.0.1/pvv_vv'
 quit_allowed = True
 stop_allowed = False
 show_tracebacks = True
@@ -6,3 +6,6 @@ input_encoding = 'utf8'
 
 low_credit_warning_limit = -100
 user_recent_transaction_limit = 100
+
+label_type = "29x90"
+label_rotate = True
