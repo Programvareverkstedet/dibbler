ab3830c494dfad6db6bf0a75d8ae9c49f4e316ba
robertem@PVV.NTNU.NO <robertem@PVV.NTNU.NO@e203dbf8-6c7f-4a35-977b-1621d8385761>
2018-04-22 15:44:24 +0000

    Fix user transaction error and other unexpected behaviour

--- text_interface/addstock.py
+++ text_interface/addstock.py
@@ -92,8 +92,8 @@ much money you're due in credits for the purchase when prompted.\n'''
 
     def perform_transaction(self):
         print 'Did you pay a different price?'
-        if self.confirm('>', default=True):
-            price = self.input_int()
+        if self.confirm('>', default=False):
+            price = self.input_int('How much did you pay?', default=self.price)
             if price > self.price:
                 print 'Illegal action, total can not be higher than your total.'
             else:
