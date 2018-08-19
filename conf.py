d4603de7bb7ff31c013250b506580d7e9113fd1e
robertem@PVV.NTNU.NO <robertem@PVV.NTNU.NO@e203dbf8-6c7f-4a35-977b-1621d8385761>
2018-08-19 13:16:49 +0000

    Fix label format for endless label types Add some more documentation to config files Create bar_codes directory to avoid IOExceptions when trying to write to non-existent directories

--- conf.py
+++ conf.py
@@ -7,5 +7,7 @@ input_encoding = 'utf8'
 low_credit_warning_limit = -100
 user_recent_transaction_limit = 100
 
+# See https://pypi.org/project/brother_ql/ for label types
+# Set rotate to False for endless labels
 label_type = "29x90"
 label_rotate = True
