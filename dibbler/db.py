f5557a1d99b96fe45ac01a53e42c82ea05273a1d
h7x4 <h7x4@nani.wtf>
2025-05-17 19:17:47 +0200

    db: handle database.url_file

--- dibbler/db.py
+++ dibbler/db.py
@@ -1,7 +1,16 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from dibbler.conf import config
 
-engine = create_engine(config.get("database", "url"))
+if (url := config.get("database", "url")) is not None:
+  database_url = url
+
+elif (url_file := config.get("database", "url_file")) is not None:
+  with Path(url_file).open() as file:
+    database_url = file.read().strip()
+
+engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
