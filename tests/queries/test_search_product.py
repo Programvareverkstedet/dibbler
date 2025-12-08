15d1763405cfb3fffdcec2eb4445d5c7a6982a94
h7x4 <h7x4@nani.wtf>
2025-12-08 21:41:13 +0900

    fixup! WIP

---
+++ tests/queries/test_search_product.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+
+from dibbler.models import Product
+from dibbler.queries.search_product import search_product
+
+def test_search_product_no_products(sql_session: Session) -> None:
+    pass
+
+def test_search_product_name_exact_match(sql_session: Session) -> None:
+    pass
+
+def test_search_product_name_partial_match(sql_session: Session) -> None:
+    pass
+
+def test_search_product_name_no_match(sql_session: Session) -> None:
+    pass
+
+def test_search_product_barcode_exact_match(sql_session: Session) -> None:
+    pass
+
+def test_search_product_hidden_products(sql_session: Session) -> None:
+    pass
+
+def test_search_product_find_hidden_products(sql_session: Session) -> None:
+    pass
