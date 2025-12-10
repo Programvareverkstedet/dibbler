919d7a5afe7040cc5158595b24e62730bed88f7c
h7x4 <h7x4@nani.wtf>
2025-12-10 15:53:45 +0900

    assert `database_url` is present

--- dibbler/db.py
+++ dibbler/db.py
@@ -5,12 +5,15 @@ from sqlalchemy.orm import sessionmaker
 
 from dibbler.conf import config
 
-if (url := config.get("database", "url")) is not None:
-  database_url = url
+database_url: str | None = None
 
+if (url := config.get("database", "url")) is not None:
+    database_url = url
 elif (url_file := config.get("database", "url_file")) is not None:
-  with Path(url_file).open() as file:
-    database_url = file.read().strip()
+    with Path(url_file).open() as file:
+        database_url = file.read().strip()
+
+assert database_url is not None, "Database URL must be specified in config"
 
 engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
