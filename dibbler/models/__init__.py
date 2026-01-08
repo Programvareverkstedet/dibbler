9ff97796e6dc6c9be0bd3716bb1659a3cab93215
h7x4 <h7x4@nani.wtf>
2026-01-08 13:52:52 +0900

    WIP: caching

--- dibbler/models/__init__.py
+++ dibbler/models/__init__.py
@@ -1,13 +1,19 @@
 __all__ = [
     "Base",
+    "LastCacheTransaction",
     "Product",
+    "ProductCache",
     "Transaction",
     "TransactionType",
     "User",
+    "UserCache",
 ]
 
 from .Base import Base
+from .LastCacheTransaction import LastCacheTransaction
 from .Product import Product
+from .ProductCache import ProductCache
 from .Transaction import Transaction
 from .TransactionType import TransactionType
 from .User import User
+from .UserCache import UserCache
